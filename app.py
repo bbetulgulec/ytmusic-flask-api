dede4e4822b7f28fb41afd42656ac94c97a98f72
Betül Güleç <gulecbetul34@gmail.com>
2025-03-16 15:43:08 +0300

    e url kısmı

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ import yt_dlp
 
 app = Flask(__name__)
 
-# Önceden belirlenmiş linkler (senin verdiğin sahte linkler)
-custom_links = {
-    "Şarkı 1": "https://rr2---sn-u0g3uxax3-xncz.googlevideo.com/videoplayback?expire=1742137974&ei=FpbWZ6DXKNj-xN8PiKPw2A8&itag=251",
-    "Şarkı 2": "https://rr2---sn-u0g3uxax3-xncz.googlevideo.com/videoplayback?expire=1742137979&ei=G5bWZ9aRCa3yi9oP8Le1-QE&itag=251"
-}
-
-# Proxy ayarı (kendi IP adresini buraya yaz)
-PROXY_URL = "http://10.0.0.151:8080"
-
 def get_top100():
     playlist_url = "https://music.youtube.com/playlist?list=PL4fGSI1pDJn5tdVDtIAZArERm_vv4uFCR"
     ydl_opts = {
@@ -19,8 +10,7 @@ def get_top100():
         'extract_flat': True,
         'force_generic_extractor': True,
         'sleep_interval': 5,
-        'max_sleep_interval': 20,
-        'proxy': PROXY_URL  # Proxy kullanarak bağlan
+        'max_sleep_interval': 20
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -29,19 +19,11 @@ def get_top100():
     tracks = []
     if 'entries' in info:
         for entry in info['entries']:
-            title = entry.get('title', 'Bilinmeyen Şarkı')
-            artist = entry.get('uploader', 'Bilinmeyen Sanatçı')
-
-            # Eğer özel URL varsa, onu kullan; yoksa YouTube Music linkini ekle
-            if title in custom_links:
-                audio_url = custom_links[title]
-            else:
-                audio_url = f"https://music.youtube.com/watch?v={entry.get('id', '')}"
-
+            video_url = f"https://music.youtube.com/watch?v={entry.get('id', '')}"
             tracks.append({
-                'title': title,
-                'artist': artist,
-                'audioUrl': audio_url
+                'title': entry.get('title', 'Bilinmeyen Şarkı'),
+                'artist': entry.get('uploader', 'Bilinmeyen Sanatçı'),
+                'videoUrl': video_url
             })
     return tracks
 
